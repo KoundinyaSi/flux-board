6822265fe7eb1ea42829dbf28b360138850bbcec
Sitaram <sitaram.28900@gmail.com>
2025-03-27 04:56:40 +0530

    Fixed build errors

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  // assetPrefix: process.env.NODE_ENV === 'production' ? '/share-on-socials/' : '',
+  // output: 'export'
 };
 
 export default nextConfig;
